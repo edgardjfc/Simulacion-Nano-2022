a5f4bbdf2406852c5e85a52d1eec3a10260f35f5
edgardjfc <98036240+edgardjfc@users.noreply.github.com>
2022-01-25 19:42:07 -0600

    UpdateP0.py
    Homework update

--- P0/P0.py
+++ P0/P0.py
@@ -1,3 +1,5 @@
+import random
+
 x = 21
 y = 3
 z = 7
@@ -7,3 +9,14 @@ var = [ x, y, z ]
 print(( x + 2)*( y**3 )*( 2 * z ))
 
 print(sum(var))
+
+myRandom = random.randint(0,30)
+
+print(myRandom)
+
+if myRandom < y:
+    print("smol")
+elif myRandom > x:
+    print("lorge")
+else:
+    print("avererage")
