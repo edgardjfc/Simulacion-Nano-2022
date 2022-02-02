1e94302bbc09c232041c45c400749f8f50668fb8
edgardjfc <98036240+edgardjfc@users.noreply.github.com>
2022-02-02 12:09:06 -0600

    DesdeHasta
    practica

---
+++ P0/desdehasta.py
@@ -0,0 +1,12 @@
+from random import randint
+desde = 1
+hasta = 1000
+menor = 5
+mayor = 15
+for k in range(menor, mayor + 1):
+    n=2**k
+    lista=[ randint(desde, hasta) for i in range(n) ]
+    lista.sort()
+    print(n, lista[0], lista[-1])
+    
+
